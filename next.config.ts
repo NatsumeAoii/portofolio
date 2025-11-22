3dfb12d7b21be20c21c85cc20327646f8970dd57
NatsumeAoii <wardanadm2@gmail.com>
2025-11-22 19:17:05 +0700

    fix: add basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/portofolio',
   images: {
     unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
   },
